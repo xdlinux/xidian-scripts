bff0e6aaa14d4c2e4263b219b5bbaec0dd335b3f
Frank <frankli0324@hotmail.com>
2019-01-17 02:08:53 +0800

    添加ids验证模块

---
+++ ids/authentication.py
@@ -0,0 +1,25 @@
+import requests
+import re
+
+from ids.GLOBAL import *
+
+
+def get_login_session(target, username, password):
+    ses = requests.session()
+    ses.headers = HEADER
+    page = ses.get(
+        'http://ids.xidian.edu.cn/authserver/login',
+        params={'service': target}
+    ).text
+    page = re.sub(REGEX_HTML_COMMENT,'',page)
+    params = {i[0]: i[1] for i in re.findall(REGEX_HIDDEN_TAG, page)}
+    ses.post(
+        'http://ids.xidian.edu.cn/authserver/login',
+        params={'service': target},
+        data=dict(params,**{
+            'username': username,
+            'password': password
+        })
+    )
+    return ses
+
