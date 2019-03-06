65f7b677b05274cab858d28cca998ea20ec099bb
Frank <frankli0324@hotmail.com>
2019-03-06 17:58:02 +0800

    fix wierd problem

--- Python/export_timetable.py
+++ Python/export_timetable.py
@@ -140,6 +140,8 @@ for week_cnt in range(len(courseList)):
     for day_cnt in range(len(courseList[week_cnt])):
         for course in courseList[week_cnt][day_cnt]:
             e = Event()
+            if course['sectionSpan'][0] > 10 :
+                continue
             e.add(
                 "description",
                 '课程名称：' + course['name'] +
