2cd7262575cf47b228ac6c2b49e29754450ba440
Frank <frankli0324@hotmail.com>
2019-08-13 11:49:00 +0800

    add license; add macOS.sh

--- Bash/chmirror/macOS.sh
+++ Bash/chmirror/macOS.sh
@@ -14,18 +14,19 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with xidian-scripts. If not, see <http://www.gnu.org/licenses/>.
 
-mirror_site="https://linux.xidian.edu.cn/mirrors/ubuntu/"
-alternative="https://mirrors.tuna.tsinghua.edu.cn/ubuntu/"
+m='https://linux.xidian.edu.cn/mirrors/homebrew/brew.git'
+a='https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git'
 
-function get_mirror() {
-    status=`curl -I -m 10 -o /dev/null -s -w %{http_code} $mirror_site`
-    if [ $status -ne '200' ];then
-        echo $alternative
-    else
-        echo $mirror_site
-    fi
-}
+cwd=`pwd`
+cd "$(brew --repo)"
+git remote set-url origin `if git ls-remote $m >/dev/null 2>&1;then echo $m;else echo $a;fi`
+cd $cwd
 
-to_use=`get_mirror`
-sudo sed -i "s/archive.ubuntu.com/$to_use/g" /etc/apt/sources.list
-# apt update
+m='https://linux.xidian.edu.cn/mirrors/homebrew-bottles/'
+a='https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles'
+status=`curl -I -m 10 -o /dev/null -s -w %{http_code} $m`
+if [ $status -ne '200' ];then
+    export HOMEBREW_BOTTLE_DOMAIN=$a
+else
+    export HOMEBREW_BOTTLE_DOMAIN=$m
+fi
