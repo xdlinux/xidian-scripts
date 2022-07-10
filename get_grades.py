4412eea329b2166d42f1b50dab28d73d1f5a1290
David "SuperBart" Chen <Songze@live.cn>
2022-07-10 23:39:40 +0800

    Update get_grades.py

--- get_grades.py
+++ get_grades.py
@@ -59,7 +59,9 @@ if '--all-terms' not in sys.argv[1:]:
     })
 
 total = [0, 0]
-course_ignore = ['形势与政策', '创业基础', '新生', '写作与沟通', '学科导论', '心理']
+course_ignore = ['军事', '形势与政策', '创业基础', '新生', '写作与沟通', '学科导论', '心理', '物理实验']
+types_ignore = ['公共任选课', '集中实践环节', '拓展提高', '通识教育核心课', '专业选修课']
+unpassed_course = {}
 
 for i in ses.post(
     'http://ehall.xidian.edu.cn/jwapp/sys/cjcx/modules/cjcx/xscjcx.do',
@@ -71,15 +73,35 @@ for i in ses.post(
         'pageNumber': 1
     }
 ).json()['datas']['xscjcx']['rows']:
-    if all((kw not in i["XSKCM"] for kw in course_ignore)):  # 不计入均分
+
+    flag = 0
+
+    for lx in types_ignore:
+        if i["KCLBDM_DISPLAY"].find(lx) != -1:
+            flag = 1
+            break
+
+    for kw in course_ignore:
+        if i["XSKCM"].find(kw) != -1:
+            flag = 1
+            break
+
+    if flag == 1:
         i["XSKCM"] = '*' + i["XSKCM"]
-    elif i["SFJG"] == '1':  # 及格
-        if i["CXCKDM"] == '01':  # 初修
-            total[0] += i["XF"] * i["ZCJ"]
-            total[1] += i["XF"]
-        else:
-            total[0] += i["XF"] * 60.0
-            total[1] += i["XF"]
+    else:
+        if i["SFJG"] == '1':  # 及格
+            if i["CXCKDM"] == '01':  # 初修
+                total[0] += i["XF"] * i["ZCJ"]
+                total[1] += i["XF"]
+            else:
+                total[0] += i["XF"] * 60.0
+                total[1] += i["XF"]
+
+    if i["SFJG"] == '0' and i["KCXZDM_DISPLAY"] == "必修":
+        unpassed_course[i["KCH"]] = i["XF"]
+    elif i["SFJG"] == '1' and i["KCH"] in unpassed_course.keys():
+        del unpassed_course[i["KCH"]]
+
     roman_nums = str.maketrans({
         'Ⅰ': 'I', 'Ⅱ': 'II', 'Ⅲ': 'III', 'Ⅳ': 'IV', 'Ⅴ': 'V', 'Ⅵ': 'VI', 'Ⅶ': 'VII', 'Ⅷ': 'VIII',
     }) # 一些终端无法正确打印罗马数字
@@ -88,8 +110,10 @@ for i in ses.post(
         f'{i["XNXQDM"]} [{i["KCH"]}]{i["XSKCM"]} '
         f'{i["KCXZDM_DISPLAY"]} '
         f'{i["ZCJ"] if i["ZCJ"] else "还没出成绩"} '
-        f'{i["XFJD"] if i["XFJD"] else ""}'
+        f'{i["KCLBDM_DISPLAY"]}'
     )
 
-print(f'加权平均成绩：{total[0] / total[1]:.2f}')
-print('注：标记有*的课程不计入均分')
+print(f'未获得的学分有：{sum(unpassed_course.values())}')
+if '--all-terms' in sys.argv[1:]:
+    print(f'加权平均成绩：{total[0] / total[1]:.2f}')
+    print('注：标记有*的课程以及未通过科目不计入均分')
