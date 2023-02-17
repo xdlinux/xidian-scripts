d3a535187b71d4873917fcca8d495f57691b4373
David "SuperBart" Chen <Songze@live.cn>
2023-02-17 14:32:28 +0800

    feat: use ids
    Because of http://tybjxgl.xidian.edu.cn/

--- choice_pe_class.py
+++ choice_pe_class.py
@@ -15,49 +15,50 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with xidian-scripts.  If not, see <http://www.gnu.org/licenses/>.
 
+import pkg_resources
+import subprocess
+import sys
+import os
+try:
+    pkg_resources.require(('libxduauth'))
+except (pkg_resources.DistributionNotFound, pkg_resources.VersionConflict):
+    subprocess.check_call([
+        sys.executable, '-m', 'pip', 'install', 'libxduauth'
+    ])
+
+try:
+    import credentials
+    USERNAME = credentials.IDS_USERNAME
+    PASSWORD = credentials.IDS_PASSWORD
+except ImportError:
+    USERNAME, PASSWORD = [os.getenv(i) for i in ('IDS_USER', 'IDS_PASS')]
+if not USERNAME or not PASSWORD:
+    print('请设置环境变量 IDS_USER 和 IDS_PASS')
+    exit(1)
+
 import time
-from io import BytesIO
-from PIL import Image
-from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad
-from requests import Session
-from bs4 import BeautifulSoup
-from base64 import b64encode
+from libxduauth import IDSSession
 
-def encrypt(v, k):
-    crypt = AES.new(k, AES.MODE_ECB)
-    return b64encode(crypt.encrypt(pad(v,8,'pkcs7')))
 
-def process(username, password):
-    whatever = Session()
-    login_page = whatever.get("https://xdkd.boxkj.com/admin/login").content
-    soup = BeautifulSoup(login_page, 'html.parser')
-    the_hash = soup.find('input',id="KEY").get('value')
-    code = whatever.get("http://xdkd.boxkj.com/code/captcha-image").content
-    Image.open(BytesIO(code)).show()
-    code = input("验证码输入：")
-    to_post = {
-            "uname": encrypt(username.encode(), the_hash.encode()),
-            "pwd": encrypt(password.encode(), the_hash.encode()),
-            "code": code
-        }
-    print(to_post)
-    result = whatever.post(
-        "https://xdkd.boxkj.com/admin/login",
-        data=to_post
-    )
-    print(result.json())
-    whatever.post("https://xdkd.boxkj.com/admin/loginUser")
-    classes = whatever.post("http://xdkd.boxkj.com/admin/chooseCurriculum/showTeachingCurriculum").json()["data"]
+def process():
+    whatever = IDSSession("http://tybjxgl.xidian.edu.cn", USERNAME, PASSWORD)
+    classes = whatever.post(
+        "http://tybjxgl.xidian.edu.cn/admin/"
+        "chooseCurriculum/showTeachingCurriculum"
+    ).json()["data"]
     for i in classes:
-        print(f'{str(i["id"])} {i["sysUserName"]} {i["teachingCurriculumName"]} {i["teachingSchoolTimeName"]}')
+        print(
+            f'{str(i["id"])} {i["sysUserName"]} '
+            f'{i["teachingCurriculumName"]} {i["teachingSchoolTimeName"]}'
+        )
     ohyeah = input("输入你想上课程的id: ")
-    choice = whatever.post(f"http://xdkd.boxkj.com/admin/stuTeacherCurriculum/chooseTeachingCurriculum?teaCurriculumid={ohyeah}&_={int(round(time.time() * 1000))}")
+    choice = whatever.post(
+        "http://tybjxgl.xidian.edu.cn/admin//"
+        "stuTeacherCurriculum/chooseTeachingCurriculum?"
+        f"teaCurriculumid={ohyeah}&_={int(round(time.time() * 1000))}"
+    )
     print(choice.content.decode())
 
-if __name__=="__main__":
-    username = input("输入帐号：")
-    password = input("输入密码：")
-    process(username,password)
 
-
+if __name__ == "__main__":
+    process()
